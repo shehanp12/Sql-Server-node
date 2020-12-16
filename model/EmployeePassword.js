41ccb67fbb1a9dfe3528b52344024f5ef0200350
shehanp12 <shehanp068@gmail.com>
2020-12-17 02:27:21 +0530

    connected to sql server

---
+++ model/EmployeePassword.js
@@ -0,0 +1,10 @@
+class EmployeePassword {
+    constructor(LoginId,Password,UserId) {
+        this.LoginId = LoginId;
+        this.Password = Password;
+        this.UserId = UserId;
+
+    }
+}
+
+module.exports = EmployeePassword;
