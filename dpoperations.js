1a50af0f4e143359dbcb08d3e99a53c79040816a
shehanp12 <shehanp068@gmail.com>
2020-12-20 02:30:13 +0530

    addEmployees function

--- dpoperations.js
+++ dpoperations.js
@@ -1,5 +1,3 @@
-
-
 const sql = require('mssql');
 const  config = require('./dbconfig');
 
@@ -35,6 +33,26 @@ async function addEvent(event) {
     }
 
 }
+
+async function addEmployee(employee) {
+
+    const pool = await sql.connect(config);
+    const  insertEmployee = await  pool.request()
+        .input('password',sql.NVarChar,employee.password)
+        .input('first_name',sql.NVarChar,employee.first_name)
+        .input('last_name',sql.NVarChar,employee.last_name)
+        .input('phone_number',sql.NVarChar,employee.phone_number)
+        .input('employee_address',sql.NVarChar,employee.employee_address)
+        .input('department_id',sql.NVarChar,employee.department_id)
+        .execute('InsertEmployeeDetails');
+    return insertEmployee.recordsets;
+
+
+
+}
+
+
+
 
 
 module.exports = {
--- routes/authentication.js
+++ routes/authentication.js
@@ -7,25 +7,75 @@ const app = express();
 const cors= require('cors');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-router.get('/order',(request,response)=>{
+// router.get('/order',(request,response)=>{
+//
+//     dboperations.getUsers().then(result => {
+//         response.json(result);
+//     })
+//
+// })
+//
+// router.get('/addJob',(request,response)=>{
+//
+//     let job = {...request.body}
+//
+//     dboperations.addJob(job).then(result => {
+//         response.status(201).json(result);
+//     })
+//
+// })
 
-    dboperations.getUsers().then(result => {
-        response.json(result);
-    })
 
-})
 
-router.get('/addJob',(request,response)=>{
+router.post('/register',async (req,res) =>{
 
-    let job = {...request.body}
+    if ((!req.body.username) || (!req.body.password)) {
+        res.json({success: false, msg: 'Enter all fields'})
+    }
+    else{
 
-    dboperations.addJob(job).then(result => {
-        response.status(201).json(result);
-    })
+        // const  emailExist = await BoardingProvider.findOne({email:req.body.email});
+        // if(emailExist) return  res.json({success: false, msg: 'Email is already exits'});
 
-})
+        // Hash Passwords
+        const  salt = await bcrypt.genSalt(10);
+
+        // const  hashPassword = await bcrypt.hash(req.body.password,salt)
+
+        // const  boardingProvider = new BoardingProvider({
+        //
+        //     username:req.body.username,
+        //     fullName:req.body.fullName,
+        //     email:req.body.email,
+        //     password:hashPassword
+        // })
+
+        const user = {...req.body}
+         dboperations.
+
+        boardingProvider.save(function (err,boardingProvider){
+            if(err){
+                res.json({success: false, msg: 'Failed to save'})
+            }
+            else {
+                res.json({success: true, msg: 'Successfully saved'})
+            }
+        })
+
+    }
+
+
+
+
+
+
+
+});
+
 
 
 module.exports=router;
